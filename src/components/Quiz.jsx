15f5e47697f6b7c7d71019ae098296c9422f7c7c
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-05-01 00:57:49 +0700

    deriving values, outputting questions and registering answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react";
+import QUESTION from "../question.js";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTION[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTION[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
